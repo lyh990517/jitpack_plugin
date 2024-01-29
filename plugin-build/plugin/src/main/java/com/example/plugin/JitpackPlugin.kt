4b27e6e330229dfe806ba5ecebbfa5ea8c2aecda
lyh990517 <lyh990517@naver.com>
2024-01-29 14:16:54 +0900

    Set Publication

--- plugin-build/plugin/src/main/java/com/example/plugin/JitpackPlugin.kt
+++ plugin-build/plugin/src/main/java/com/example/plugin/JitpackPlugin.kt
@@ -2,9 +2,29 @@ package com.example.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.get
 
 class JitpackPlugin : Plugin<Project> {
-    override fun apply(target: Project) {
-        println("jitpack")
+    override fun apply(project: Project) {
+        project.afterEvaluate {
+            extensions.findByType(org.gradle.api.publish.PublishingExtension::class.java)
+                ?.apply {
+                    publications {
+                        create<MavenPublication>("release"){
+                            from(components["release"])
+                            groupId = "com.github.your github name"
+                            artifactId = "your Repository name"
+                            version = "Same As Tag"
+
+                            pom {
+                                name.set("name")
+                                description.set("description")
+                            }
+                        }
+                    }
+                }
+        }
     }
 }
