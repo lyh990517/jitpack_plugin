d19c11dc6d2e3fcbc3c9d11888960d7a0b417478
lyh990517 <lyh990517@naver.com>
2024-01-30 13:10:01 +0900

    Add Plugin Example

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -67,4 +66,16 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+}
+
+Jitpack{
+    setJitpack{
+        create("release"){
+            githubName = "com.github.lyh990517"
+            repositoryName = "jitpack_plugin"
+            version = "1.0.0"
+            name = "plugin"
+            description = "sample"
+        }
+    }
 }
